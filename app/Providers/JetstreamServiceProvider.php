12ecb10c3ca4f75084fc15f140750a959001cd28
unknown <boubakarysaidou0705@gmail.com>
2022-02-12 17:43:11 +0100

    initialisation du projet

---
+++ app/Providers/JetstreamServiceProvider.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Providers;
+
+use App\Actions\Jetstream\DeleteUser;
+use Illuminate\Support\ServiceProvider;
+use Laravel\Jetstream\Jetstream;
+
+class JetstreamServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->configurePermissions();
+
+        Jetstream::deleteUsersUsing(DeleteUser::class);
+
+        $this->app->singleton(
+            \laravel\fortify\contracts\loginResponses::class,
+            \App\Http\Responses\loginResponses::class
+
+        );
+    }
+
+    /**
+     * Configure the permissions that are available within the application.
+     *
+     * @return void
+     */
+    protected function configurePermissions()
+    {
+        Jetstream::defaultApiTokenPermissions(['read']);
+
+        Jetstream::permissions([
+            'create',
+            'read',
+            'update',
+            'delete',
+        ]);
+    }
+}
